f165a6a791800ac95765a2cb69a0f94c110285f1
Brian <bstanton773@gmail.com>
2021-11-09 13:36:06 -0600

    Add shell context processor

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 from app import app
+from app.wrappers import TVMazeAPI
 
 
 if __name__ == '__main__':
     app.run()
+
+
+@app.shell_context_processor
+def make_context():
+    return {
+        'client': TVMazeAPI()
+    }
